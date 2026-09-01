ab7a6ca2abf5079adb42cb95180f42a53f1ed735
GIT <systemlogic@users.noreply.github.com>
2026-09-01 16:40:50 +0530

    Add: kth-largest-integer-in-a-stream - submission-3

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-3.py
@@ -0,0 +1,18 @@
+import heapq
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.arr = nums
+        self.k = k
+        heapq.heapify(self.arr)
+        while len(self.arr) > k:
+            heapq.heappop(self.arr)
+
+        
+    def add(self, val: int) -> int:
+        heapq.heappush(self.arr, val)
+
+        if len(self.arr) > self.k:
+            heapq.heappop(self.arr)
+        return self.arr[0]
+

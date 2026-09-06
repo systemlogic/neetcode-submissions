a6f872a3ec47a171e8c04800481197612dc72a39
GIT <systemlogic@users.noreply.github.com>
2026-09-06 22:19:26 +0530

    Add: evaluate-reverse-polish-notation - submission-15

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-15.py
@@ -0,0 +1,21 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stk = []
+        for ch in tokens:
+            if ch not in ["+", "-", "*", "/"]:
+                stk.append(int(ch))
+
+            else:
+                num_2 = stk.pop()
+                num_1 = stk.pop()
+                
+                if ch == "*":
+                    stk.append(num_1 * num_2)
+                elif ch == "/":
+                    stk.append(int(num_1 / num_2))
+                elif ch == "+":
+                    stk.append(num_1 + num_2)
+                elif ch == "-":
+                    stk.append(num_1 - num_2)
+        print(stk[-1])
+        return stk[-1]
